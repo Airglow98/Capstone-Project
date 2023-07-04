99bc1ade5e396fad254aa2ae29a847111d61af26
Niklas Wieland <niklaswielandhotmail.de>
2023-07-04 15:49:04 +0200

    does localstorage works?

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,24 @@
 import GlobalStyle from "../styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { initialGames } from "@/db/games/data";
 
 export default function App({ Component, pageProps }) {
-  const [games, setGames] = useState(
-    initialGames.map((game) => ({
-      ...game,
-      isLibrary: false,
-      isWishlist: false,
-    }))
-  );
+  const [games, setGames] = useState(() => {
+    const storedGames = JSON.parse(localStorage.getItem("games"));
+    if (storedGames) {
+      return storedGames;
+    } else {
+      return initialGames.map((game) => ({
+        ...game,
+        isLibrary: false,
+        isWishlist: false,
+      }));
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("games", JSON.stringify(games));
+  }, [games]);
 
   function toggleIsLibrary(id) {
     setGames(
