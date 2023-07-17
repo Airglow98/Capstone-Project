e41bb884fd9d9a60a8bb86aa136b97c7bcaa4f5e
Niklas Wieland <niklaswielandhotmail.de>
2023-07-17 12:00:46 +0200

    some styling + first steps to the searchbar bugfix

--- components/Searchbar/index.js
+++ components/Searchbar/index.js
@@ -2,19 +2,18 @@ import { useState } from "react";
 import styled from "styled-components";
 
 export default function Searchbar({ games, setGames }) {
-  const [gamesNotFound, setGamesNotFound] = useState();
+  const [gamesNotFound, setGamesNotFound] = useState(false);
+  const [searchGames, setSearchGames] = useState("");
+
   function handleOnChange(event) {
-    const formData = new FormData(event.target.form);
-    const data = Object.fromEntries(formData);
+    const query = event.target.value;
+    setSearchGames(query);
+
     const filteredGames = games.filter((game) =>
-      game.title.toLowerCase().includes(data.searchbar.toLowerCase())
+      game.title.toLowerCase().includes(query.toLowerCase())
     );
     setGames(filteredGames);
-    if (filteredGames.length === 0) {
-      setGamesNotFound(true);
-    } else {
-      setGamesNotFound(false);
-    }
+    setGamesNotFound(filteredGames.length === 0);
   }
 
   return (
@@ -26,6 +25,7 @@ export default function Searchbar({ games, setGames }) {
           id="searchbar"
           name="searchbar"
           onChange={handleOnChange}
+          value={searchGames}
         />
         <StyledSvg
           xmlns="http://www.w3.org/2000/svg"
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import NavBar from "../../components/NavBar/index";
 import LibraryLink from "../../components/LibraryLink";
 import ImageUploadForm from "../../components/ImageUploadForm";
+import Image from "next/image";
 
 export default function ProfilePage({
   amountOfLibraryGames,
@@ -23,17 +24,29 @@ export default function ProfilePage({
   return (
     <>
       <Header HeaderText={"My Profile"} />
+      <StyledDiv>
+        <StyledImage
+          src={
+            "http://res.cloudinary.com/da0yuort3/image/upload/v1689343619/46dcf2521d59b62f3aab2e700.png"
+          }
+          alt={"profile image"}
+          width={150}
+          height={150}
+        />
+      </StyledDiv>
+      <StyledDiv2>
+        <p>Owned Games: {amountOfLibraryGames}</p>
+        <p> Wishlist: {amountOfWishlistGames}</p>
+      </StyledDiv2>
+      <StyledFlexBox>
+        <LibraryLink />
+        <WishlistLink />
+      </StyledFlexBox>
       <StyledProfileHeader>
         <StyledUpload>
           <ImageUploadForm />
         </StyledUpload>
       </StyledProfileHeader>
-      <p>Owned Games: {amountOfLibraryGames}</p>
-      <p>Wishlist: {amountOfWishlistGames}</p>
-      <StyledDiv>
-        <LibraryLink />
-        <WishlistLink />
-      </StyledDiv>
       <NavBar />
     </>
   );
@@ -46,7 +59,7 @@ const StyledProfileHeader = styled.div`
   margin-right: 10vw;
 `;
 
-const StyledDiv = styled.div`
+const StyledFlexBox = styled.div`
   display: flex;
   justify-content: space-around;
 `;
@@ -55,4 +68,21 @@ const StyledUpload = styled.div`
   width: 80%;
   padding: 1rem;
   margin-left: 10%;
+`;
+
+const StyledDiv = styled.div`
+  margin-top: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10vh;
+`;
+
+const StyledImage = styled(Image)`
+  border-radius: 50%;
+`;
+
+const StyledDiv2 = styled.div`
+  margin-left: 14vw;
+  margin-bottom: 5vh;
 `;
